a33e5e618c1da369cb0dc449ae18524404b38029
MotoMotoRoto <MotoMotoRoto_Roto000@hotmail.com>
2024-09-08 15:11:03 -0700

    cambios

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+document.getElementById('searchInput').addEventListener('keyup', function () {
+    let filter = this.value.toLowerCase();
+    let listItems = document.querySelectorAll('#itemList li');
+
+    listItems.forEach(function (item) {
+        let text = item.textContent.toLowerCase();
+        if (text.includes(filter)) {
+            item.style.display = '';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
